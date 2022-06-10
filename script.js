7def6e7730a36a7df530739a352ad79857fb60c3
theor98 <100685679+theor98@users.noreply.github.com>
2022-06-10 17:07:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ d3.selection.prototype.moveToFront = function() {
     });
   }; 
 
-d3.csv('Données/51_csv_final.csv',function(d){
+d3.csv('./Données/51_csv_final.csv',function(d){
     return {
                nom : d.nom,
                genre : d.genre,
